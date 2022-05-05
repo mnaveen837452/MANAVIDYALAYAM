d7dc2e8bd70d7f54007088c9a880d2297f0c7c7c
Naveen <naveenkumarvrp@gmai.com>
2022-05-05 15:27:57 +0530

    Added basic project structure

---
+++ src/main/java/com/manavidyalayam/ManavidyalayamApplication.java
@@ -0,0 +1,13 @@
+package com.manavidyalayam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManavidyalayamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManavidyalayamApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/manavidyalayam/ManavidyalayamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.manavidyalayam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManavidyalayamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
